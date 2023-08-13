84c996631f26e8974ac81144047414ed67723a46
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-08-14 01:24:16 +0530

    feat - started whit signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import React,{useState} from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
+
 export default function LoginPage(){
+
    return (
-    <div className="bg-black">
-        <h1 className="text-center text-white text-2xl">Hello world</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-center text-black text-2xl">Login</h1>
     </div>
    )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 export default function SignUp() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const signUpHandler = async () => {};
+
   return (
-    <div>
-      <h1>signUp</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black">
+      <h1 className="text-center text-white text-4xl mb-10">Sign up</h1>
+      <hr />
+      <label htmlFor="name" className="text-white">Name</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        value={user.name}
+        onChange={(e) => {
+          setUser({ ...user, name: e.target.value });
+        }}
+        placeholder="Enter your Name"
+      />
+      <label htmlFor="email" className="text-white">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="email"
+        value={user.email}
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+        placeholder="Enter your Email"
+      />
+      <label htmlFor="phone" className="text-white">Phone</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="number"
+        value={user.phone}
+        onChange={(e) => {
+          setUser({ ...user, phone: e.target.value });
+        }}
+        placeholder="Enter your Phone Number"
+      />
+      <label htmlFor="password" className="text-white">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        value={user.password}
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+        placeholder="Enter your Password"
+      />
+      <button
+          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-white px-10"
+      >sign up here
+      </button>
     </div>
   );
 }
